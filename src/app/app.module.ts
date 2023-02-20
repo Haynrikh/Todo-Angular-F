af3a7c272751ca0c2e1fdddedaa2df0db2143154
Henry <haynrikh.santrosyan@optimumpartners.co>
2023-02-20 14:38:27 +0400

    backup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetTaskComponent } from './get-task/get-task.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+
+const appRoute: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'newTask', component: TaskFormComponent},
+  {path: 'task/:id', component: EditTaskComponent},
+  {path: 'user/:id', component: EditUserComponent}
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetTaskComponent,
+    HeaderComponent,
+    TaskFormComponent,
+    UserDataComponent,
+    DashboardComponent,
+    EditTaskComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [GetTaskComponent, HeaderComponent, TaskFormComponent, UserDataComponent, DashboardComponent, EditTaskComponent, EditUserComponent]
 })
 export class AppModule { }
